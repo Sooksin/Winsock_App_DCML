4ebd4f413abc223d05a610d84044abf185135559
ElbertHAN <ed.dekorte@student.han.nl>
2024-11-22 13:21:22 +0100

    Reset

---
+++ client_app/cmake-build-debug/CMakeFiles/client_app.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/client_app.dir/main.cpp.obj"
+  "CMakeFiles/client_app.dir/main.cpp.obj.d"
+  "client_app.exe"
+  "client_app.exe.manifest"
+  "client_app.pdb"
+  "libclient_app.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/client_app.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
